81f87a1d23faf8e30f29efddeff4cd7dd6553f4e
Ivan Perez <pumi0751@gmail.com>
2024-12-15 12:49:05 +0000

    feat: adding some tables(tags, articles and articlesTags)

--- src/models.py
+++ src/models.py
@@ -2,18 +2,84 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-class User(db.Model):
+class ArticlesTags(db.Model):
+    __tablename__= 'articleTags'
+    id = db.Column(db.Integer, primary_key=True)
+    article_id = db.Column(db.Integer, db.ForeignKey('articles.id'), nullable=False)
+    tag_id = db.Column(db.Integer, db.ForeignKey('tags.id'), nullable=False)
+    extra_info = db.Column(db.String(100))
+
+    def __repr__(self):
+        return '<ArticlesTags %r>' % self.email
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "article_id": self.article_id,
+            "tag_id": self.tag_id,
+            "extra_info": self.extra_info
+            # do not serialize the password, its a security breach
+        }
+    
+
+
+
+
+class Users(db.Model):
+
+    __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    articles = db.relationship('Articles', backref='user')
 
     def __repr__(self):
-        return '<User %r>' % self.username
+        return '<User %r>' % self.email
 
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
+            # do not serialize the password, its a security breach
+        }
+    
+
+class Articles(db.Model):
+    __tablename__ = 'articles'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(80), nullable=False)
+    content = db.Column(db.Text(), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    tags = db.relationship('Tags', secondary='articleTags', backref='articles')
+
+
+    def __repr__(self):
+        return '<Articles %r>' % self.id
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "title": self.title,
+            "content": self.content,
+            "user_id": self.user_id,
+            "tags": [tag.serialize() for tag in self.tags] if self.tags else None
+            # do not serialize the password, its a security breach
+        }
+
+
+class Tags(db.Model):
+    __tablename__ = 'tags'
+    id = db.Column(db.Integer, primary_key=True)
+    tag = db.Column(db.String(80), unique=False, nullable=False)
+
+
+    def __repr__(self):
+        return '<Tags %r>' % self.id
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "tag": self.tag,
             # do not serialize the password, its a security breach
         }
